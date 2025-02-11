50ddf7cb32fb0f003c4f6fb733b5503e29ea5db7
Yan Xu <yanxuok@gmail.com>
2025-02-11 10:05:19 +0800

    Initial Commit

---
+++ News Monitor/News_MonitorApp.swift
@@ -0,0 +1,32 @@
+//
+//  News_MonitorApp.swift
+//  News Monitor
+//
+//  Created by Yan Xu on 11/2/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct News_MonitorApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ News MonitorTests/News_MonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  News_MonitorTests.swift
+//  News MonitorTests
+//
+//  Created by Yan Xu on 11/2/2025.
+//
+
+import Testing
+@testable import News_Monitor
+
+struct News_MonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
